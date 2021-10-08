9e6f909fe9933ad2a184a685c1b7ea1cbebe9b2e
nundox <nund0x@protonmail.com>
2021-10-07 22:26:13 -0400

    tests are now minting :)))

--- rust/test/nft-candy-machine.ts
+++ rust/test/nft-candy-machine.ts
@@ -17,7 +17,8 @@ const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
   "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
 );
 const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
-  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  // "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  "HNq6V1U2k7S6ZQfn5JxozMMEwJGtpTnjHGUZUHemyPhA"
 );
 function fromUTF8Array(data: number[]) {
   // array of bytes
@@ -482,6 +483,8 @@ describe("nft-candy-machine", function () {
           this.candyMachineUuid
         );
         console.log(`MINT #${i}`);
+        const metadataData = connection.getAccountInfo(metadata);
+        console.log("\tmetadata:", metadataData);
         const machine: CandyMachine = await program.account.candyMachine.fetch(
           candyMachine
         );
--- rust/token-metadata/program/src/lib.rs
+++ rust/token-metadata/program/src/lib.rs
@@ -11,4 +11,5 @@ pub mod utils;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
+//solana_program::declare_id!("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
+solana_program::declare_id!("HNq6V1U2k7S6ZQfn5JxozMMEwJGtpTnjHGUZUHemyPhA");
